7c9da12a06667f61d67b889af960576bb48a0a94
praveen <praveen904265@gmail.com>
2025-01-22 21:31:38 +0530

    Initial Commit

---
+++ Study Material/Study_MaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_MaterialApp.swift
+//  Study Material
+//
+//  Created by praveen on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Study_MaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
